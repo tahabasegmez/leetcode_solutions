688d69c90d590fbcccd18250e10a96a090c143dd
Halil Taha Basegmez <tahabasegmez@gmail.com>
2026-09-11 15:32:58 +0300

    climbing stairs

---
+++ ClimbingStairs.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int[] steps = new int[n + 1];
+
+        steps[1] = 1;
+        steps[2] = 2;
+        
+
+        for (int i = 3; i <= n; i++) {
+            steps[i] = steps[i - 1] + steps[i - 2];
+        }
+
+        return steps[n];
+    }
+}
